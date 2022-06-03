e5d48c52d7c7f4a085e6d5cb7d9a60c41f2c91bb
fantasma006 <105890463+fantasma006@users.noreply.github.com>
2022-06-03 19:00:01 -0300

    Add files via upload
    Os arquivos foram importados para o meu repositório remoto

---
+++ avaliacaogit.php
@@ -0,0 +1,10 @@
+<html> 
+    <form method = "post"> 
+        Digite seu nome: 
+        <input type = "text" name = "v1">
+        Digite o nome do seu amor: 
+        <input type = "text" name = "v2"> 
+
+<?php
+$a = rand(0,100);
+?>
